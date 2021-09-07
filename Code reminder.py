a5f8595bf68870aa452688a632d206a69195cd6a
Value Digger <76144760+valuediggger@users.noreply.github.com>
2021-09-08 00:19:25 +0530

    Add files via upload

---
+++ Code reminder.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+    notification.notify(
+        title ="**Code everyday buddy!!!",
+        message = "Compunding takes time but gives tremendous return in Long-term",
+        timeout = 12
+    )
+
+
+# In[ ]:
+
+
+
+
---
+++ Code_reminder.bat
@@ -0,0 +1,2 @@
+"C:\Users\Ankit\Anaconda3\python.exe" "C:\Users\Ankit\Desktop\Sources\Python Notebooks\Code reminder.py"
+pause
